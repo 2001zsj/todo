0a5e16830f0eb76238533e6b4c6ee51bf7072c59
2001zsj <2001zsj@users.noreply.github.com>
2026-06-09 08:41:42 +0800

    🐛 升级SW缓存版本，解决字体修改被旧缓存拦截

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='hoshi-todo-v1';
+const CACHE_NAME='hoshi-todo-v2';
 const ASSETS=['./index2.html','./manifest2.json','./icon2.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim()});
