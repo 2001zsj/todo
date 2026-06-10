c9cde8ac7b88635eacfee335933bd1a934e01da5
2001zsj <2001zsj@users.noreply.github.com>
2026-06-10 15:39:29 +0800

    ⏱ 开屏时间延长至2.8秒

--- android/app/src/main/java/com/maho/todo/MainActivity.java
+++ android/app/src/main/java/com/maho/todo/MainActivity.java
@@ -20,7 +20,7 @@ import androidx.core.view.WindowInsetsControllerCompat;
 
 public class MainActivity extends Activity {
     private static final String APP_URL = "https://2001zsj.github.io/todo/";
-    private static final int MIN_SPLASH_MS = 1800; // 最少展示1.8秒
+    private static final int MIN_SPLASH_MS = 2800; // 最少展示2.8秒
     private WebView webView;
     private View splashView;
     private boolean pageReady = false;
